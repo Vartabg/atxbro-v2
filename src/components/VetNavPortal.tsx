ffef3c3f0404ca033800fb3fd9b737451b89a896
Vartabg <garo.vartabed@gmail.com>
2025-06-05 22:54:23 -0500

    debug: Add visual border to VetNavPortal to identify render path

--- src/components/VetNavPortal.tsx
+++ src/components/VetNavPortal.tsx
@@ -1,9 +1,11 @@
 'use client';
+
 import VetNavMap from './VetNavMap';
 
 const VetNavPortal = ({ onNavigateToApp }) => {
   return (
-    <div className="w-full h-full bg-gray-900">
+    // This div will now have a bright red border for debugging
+    <div className="w-full h-full bg-gray-900" style={{ border: '5px solid red' }}>
       <VetNavMap onSelectState={onNavigateToApp} />
     </div>
   );
